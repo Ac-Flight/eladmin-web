27fe50c44f03006ae8146062f00dc0c9d11ef7d9
haohuisheng <641424370@qq.com>
2021-09-27 14:24:26 +0800

    人员管理

--- src/views/base/MaintenanceProject.vue
+++ src/views/base/MaintenanceProject.vue
@@ -1,5 +1,16 @@
 <template>
-  <div />
+  <div class="app-container">
+    <!--工具栏-->
+    <div class="head-container">
+      <div>
+        <!-- 搜索 -->
+        <el-input clearable size="small" placeholder="模糊搜索" style="width: 200px;" class="filter-item" />
+        <date-range-picker class="date-item" />
+        <rrOperation />
+      </div>
+      <crudOperation :permission="permission" />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +19,13 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style rel="stylesheet/scss" lang="scss" scoped>
+::v-deep .el-input-number .el-input__inner {
+  text-align: left;
+}
+::v-deep .vue-treeselect__control,::v-deep .vue-treeselect__placeholder,::v-deep .vue-treeselect__single-value {
+  height: 30px;
+  line-height: 30px;
+}
 </style>
+
