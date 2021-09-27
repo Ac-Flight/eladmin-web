87ddddf8df3bc17aef7dc4d0ce5eacbd2bdc93be
epicParsec@outlook.com <166Me2431>
2021-09-27 21:05:27 +0800

    页面

---
+++ src/views/qcm/accm/ControlCardManagement.vue
@@ -0,0 +1,13 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  name: 'ControlCardManagement'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/qcm/aohm/HandoverApplication.vue
@@ -0,0 +1,13 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  name: 'HandoverApplication'
+}
+</script>
+
+<style scoped>
+
+</style>
