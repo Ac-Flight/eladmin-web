9713d4b3ffc4b21778227c12a5892bb44904f82c
haohuisheng <641424370@qq.com>
2021-09-27 14:01:22 +0800

    基础信息管理

---
+++ src/views/base/DrawingType.vue
@@ -0,0 +1,13 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  name: 'DrawingType'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/base/MaintenanceType.vue
@@ -0,0 +1,13 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  name: 'MaintenanceType'
+}
+</script>
+
+<style scoped>
+
+</style>
